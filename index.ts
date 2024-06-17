604b9b0d0804f1e9c16a53a4aefd2ab5807b1393
Abdullah Abdulkareem <channel.redrex@gmail.com>
2024-06-18 01:06:51 +0300

    Expose Authorization header

--- index.ts
+++ index.ts
@@ -12,7 +12,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    exposedHeaders: ["Authorization"],
+  })
+);
 
 app.get("/", (req, res) => {
   return res.send("Post Room");
